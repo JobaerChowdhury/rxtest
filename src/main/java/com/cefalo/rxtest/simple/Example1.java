80cb7ab1dfe90ebfd222010627313b6d2c0c9c14
jobaer <jobaer.du@gmail.com>
2016-03-30 20:09:10 +0600

    Some more basic example

--- src/main/java/com/cefalo/rxtest/simple/Example1.java
+++ src/main/java/com/cefalo/rxtest/simple/Example1.java
@@ -4,13 +4,50 @@ import rx.Observable;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class Example1 {
     public static void main(String[] args) throws InterruptedException {
-        basicEvent();
+        basicFlatMap();
+
+        Thread.sleep(10 * 1000);
     }
+
+    private static void basicOperations() {
+        List<Integer> integers = Arrays.asList(2, 1, 3, 4, 5, 6, 7, 8);
+
+        Observable<Integer> numbers = Observable.from(integers);
+        numbers.subscribe(num -> {
+            System.out.println("Number is " + num);
+        });
+
+        Observable<Integer> filteredObservables = numbers.filter(integer -> integer % 2 == 0);
+        filteredObservables.subscribe(System.out::println);
+
+        Observable<String> ids = numbers.map(num -> {
+            return "Id: " + num;
+        });
+        ids.subscribe(System.out::println);
+    }
+
+    private static void basicFlatMap() {
+        List<Integer> integers = Arrays.asList(2, 1, 3, 4, 5, 6, 7, 8);
+        Observable<Integer> numbers = Observable.from(integers);
+
+        Observable<Person> personObservable = numbers.flatMap(num -> {
+            return getPersonById(num);
+        });
+
+        personObservable.subscribe(System.out::println);
+    }
+
+    private static Observable<Person> getPersonById(int id) {
+        Random random = new Random();
+        int delay = random.nextInt(5);
+        return Observable.just(new Person(id)).delay(delay, TimeUnit.SECONDS);
+    };
 
     private static void basicEvent() throws InterruptedException {
         Observable<Long> interval = Observable.interval(1, TimeUnit.SECONDS);
---
+++ src/main/java/com/cefalo/rxtest/simple/Person.java
@@ -0,0 +1,14 @@
+package com.cefalo.rxtest.simple;
+
+public class Person {
+    private int id;
+
+    public Person(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [" + this.id + "]";
+    }
+}
