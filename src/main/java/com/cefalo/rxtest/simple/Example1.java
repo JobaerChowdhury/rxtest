75e77142164e8fd4c6265103d05e907de1c1309d
JobaerChowdhury <jobaer.du@gmail.com>
2016-03-30 18:23:22 +0600

    Very simple event

--- src/main/java/com/cefalo/rxtest/simple/Example1.java
+++ src/main/java/com/cefalo/rxtest/simple/Example1.java
@@ -4,10 +4,30 @@ import rx.Observable;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class Example1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        basicEvent();
+    }
+
+    private static void basicEvent() throws InterruptedException {
+        Observable<Long> interval = Observable.interval(1, TimeUnit.SECONDS);
+        interval.subscribe(l -> {
+            System.out.println("Interval " + l);
+        });
+
+        Observable<Long> fixed = interval.take(5);
+        fixed.subscribe(l -> {
+            System.out.println("Fixed " + l);
+        });
+
+        Thread.sleep(10 * 1000);
+    }
+
+
+    private static void collectionAndRx() {
         List<Integer> integers = Arrays.asList(2, 1, 3, 4, 5, 6, 7, 8);
         List<Integer> filtered = integers.stream().filter(i -> i % 2 == 0).collect(Collectors.toList());
         System.out.println(integers);
